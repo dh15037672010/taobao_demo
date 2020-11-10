018529bd44cf2762e5434a1660350be5a8261ad5
dh971102 <2280131807@qq.com>
2020-11-10 10:44:11 +0800

    '映射名后缀省略，路径映射配置'

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     configureWebpack: {
         resolve: {
-            extensions:[],  //后缀名可以不写了。
+            //extensions:['.js','.vue','.json'],  //后缀名可以不写了。
             alias: {
                  // '@': 'src', 内部已经配过.store和router不用额外配置，只在main中引入一次
                 'assets': '@/assets',
